47f36f7b38888012abc13d748273105be62c5585
Tamu1nabin <ntamu98@gmail.com>
2021-03-17 08:46:53 +0545

    third commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/homepage','App\Http\Controllers\UserController@index')->name('homepage');
